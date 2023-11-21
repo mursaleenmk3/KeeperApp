0aac3261f6b151d012cad4c2f28ce53c881d8946
mursaleenmk3 <66430973+mursaleenmk3@users.noreply.github.com>
2023-11-20 23:22:29 -0800

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css'
+
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <p>Copyright @ 2003</p>
+        </footer>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css"
+
+export default function Header(){
+    return(
+        <div className="header">
+            <h1>Keeper</h1>
+        </div>
+    )
+}
+
